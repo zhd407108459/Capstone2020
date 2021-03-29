4535f0c0d49c74fe37a5e7b135af9644c687367f
Huadong Zhang <407108459@qq.com>
2021-03-30 03:54:03 +0800

    Integrate Clown Boss Animation

---
+++ Assets/Scripts/Enemies/ClownBossAnimatorManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClownBossAnimatorManager : MonoBehaviour
+{
+    public Animator animator;
+    public void Attack1()
+    {
+        animator.SetTrigger("Attack1");
+    }
+
+    public void Attack2()
+    {
+        animator.SetTrigger("Attack2");
+    }
+
+    public void Attack3()
+    {
+        animator.SetTrigger("Attack3");
+    }
+
+    public void Attack4()
+    {
+        animator.SetTrigger("Attack4");
+    }
+
+    public void Hurt()
+    {
+        animator.SetTrigger("Hurt");
+    }
+
+    public void Angry()
+    {
+        animator.SetBool("IsAngry", true);
+    }
+
+    public void Smile()
+    {
+        animator.SetBool("IsAngry", false);
+    }
+
+    public void Reset()
+    {
+
+    }
+}
