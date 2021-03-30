6a27cce04989cb0c92d6f6c5f2490d9cc1d7cc7d
Huadong Zhang <407108459@qq.com>
2021-03-31 01:43:36 +0800

    Integrate assets

--- Assets/Scripts/Map/BrokenPlatform.cs
+++ Assets/Scripts/Map/BrokenPlatform.cs
@@ -7,6 +7,9 @@ public class BrokenPlatform : BasicPlatform
     public int brokenTime;
     public int recoverTime;
 
+    public GameObject brokenParticlePrefab;
+    public GameObject breakingParticle;
+
     public Color normalColor;
     public Color breakingColor;
     public Color brokenColor;
@@ -18,7 +21,7 @@ public class BrokenPlatform : BasicPlatform
 
     void Start()
     {
-        
+        breakingParticle.SetActive(true);
     }
 
     void Update()
@@ -61,6 +64,15 @@ public class BrokenPlatform : BasicPlatform
     {
         isBroken = false;
         isBreaking = false;
+
+        if (breakingParticle != null)
+        {
+            ParticleSystem[] ps = breakingParticle.GetComponentsInChildren<ParticleSystem>();
+            foreach (var n in ps)
+            {
+                n.Stop();
+            }
+        }
         for (int i = 0; i < sprites.Count; i++)
         {
             sprites[i].color = normalColor;
@@ -71,6 +83,14 @@ public class BrokenPlatform : BasicPlatform
     public void Breaking()
     {
         isBreaking = true;
+        if(breakingParticle != null)
+        {
+            ParticleSystem[] ps = breakingParticle.GetComponentsInChildren<ParticleSystem>();
+            foreach(var n in ps)
+            {
+                n.Play();
+            }
+        }
         for (int i = 0; i < sprites.Count; i++)
         {
             sprites[i].color = breakingColor;
@@ -85,6 +105,18 @@ public class BrokenPlatform : BasicPlatform
             sprites[i].color = brokenColor;
         }
         breakTimer = 0;
+        if (breakingParticle != null)
+        {
+            ParticleSystem[] ps = breakingParticle.GetComponentsInChildren<ParticleSystem>();
+            foreach (var n in ps)
+            {
+                n.Stop();
+            }
+        }
+        if (brokenParticlePrefab != null)
+        {
+            Instantiate(brokenParticlePrefab, sprites[0].transform.position, Quaternion.identity);
+        }
         GameManager.instance.player.GetComponent<PlayerGridMovement>().CheckYPosition();
     }
 }
