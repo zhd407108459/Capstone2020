a1aa1949f897a31efecc62eff994d84977748c42
Huadong Zhang <407108459@qq.com>
2021-04-26 23:01:30 -0400

    Post-Process

---
+++ Assets/Scripts/Lighting/DirectionalLightLengthAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DirectionalLightCaster2D))]
+public class DirectionalLightLengthAnimator : MonoBehaviour
+{
+    public float speed;
+
+    private float target;
+    private float lastLength;
+    private bool isAnimating;
+
+    private void Update()
+    {
+        if (!GameManager.instance.isPaused && isAnimating)
+        {
+            DirectionalLightCaster2D light = GetComponent<DirectionalLightCaster2D>();
+            if((light.length - target)*(lastLength - target) <= 0)
+            {
+                light.length = target;
+                isAnimating = false;
+            }
+            light.length += ((target - light.length) > 0 ? 1 : -1) * speed * Time.deltaTime;
+            lastLength = light.length;
+        }
+    }
+
+    public void AnimateToLength(float targetLength)
+    {
+        target = targetLength;
+        isAnimating = true;
+    }
+}
---
+++ Assets/Scripts/Lighting/PointLightSizeAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PointLightCaster2D))]
+public class PointLightSizeAnimator : MonoBehaviour
+{
+    public float speed;
+
+    private float target;
+    private float lastLength;
+    private bool isAnimating;
+
+    private void Update()
+    {
+        if (!GameManager.instance.isPaused && isAnimating)
+        {
+            PointLightCaster2D light = GetComponent<PointLightCaster2D>();
+            if ((light.size - target) * (lastLength - target) <= 0)
+            {
+                light.size = target;
+                isAnimating = false;
+            }
+            light.size += ((target - light.size) > 0 ? 1 : -1) * speed * Time.deltaTime;
+            lastLength = light.size;
+        }
+    }
+
+    public void AnimateToSize(float targetSize)
+    {
+        target = targetSize;
+        isAnimating = true;
+    }
+}
