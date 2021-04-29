602e118eaa5840aba4e94be01addc0be49580e28
Huadong Zhang <407108459@qq.com>
2021-04-29 00:44:31 -0400

    Integrate Cutscene Sounds

---
+++ Assets/Scripts/FMODEventPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class FMODEventPlayer : MonoBehaviour
+{
+    public string path;
+    public bool isBGM;
+
+    private EventInstance sound;
+
+    public void Play()
+    {
+        sound = RuntimeManager.CreateInstance(path);
+        if (SettingManager.instance != null)
+        {
+            sound.setVolume(SettingManager.instance.overAllVolume * (isBGM ? SettingManager.instance.musicVolume : SettingManager.instance.soundEffectVolume));
+        }
+        sound.start();
+    }
+
+    public void Stop()
+    {
+        if (sound.isValid())
+        {
+            sound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+    }
+}
