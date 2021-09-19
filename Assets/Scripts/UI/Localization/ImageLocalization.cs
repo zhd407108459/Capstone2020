ef46dfb2d53186060dfb5f0c319f55fc67c9ddf6
Huadong Zhang <407108459@qq.com>
2021-09-18 22:23:08 -0400

    1

---
+++ Assets/Scripts/UI/Localization/ImageLocalization.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Image target;
+    public List<Sprite> sources = new List<Sprite>();
+
+    private void OnEnable()
+    {
+        ApplyLanguage();
+    }
+
+    void Start()
+    {
+    }
+
+    public void ApplyLanguage()
+    {
+        if (SettingManager.instance != null)
+        {
+            if (SettingManager.instance.language < sources.Count)
+            {
+                target.sprite = sources[SettingManager.instance.language];
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Localization/SpriteRendererLocalization.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRendererLocalization : MonoBehaviour
+{
+    public SpriteRenderer target;
+    public List<Sprite> sources = new List<Sprite>();
+
+    private void OnEnable()
+    {
+        ApplyLanguage();
+    }
+
+    void Start()
+    {
+    }
+
+    public void ApplyLanguage()
+    {
+        if (SettingManager.instance != null)
+        {
+            if (SettingManager.instance.language < sources.Count)
+            {
+                target.sprite = sources[SettingManager.instance.language];
+            }
+        }
+    }
+}
