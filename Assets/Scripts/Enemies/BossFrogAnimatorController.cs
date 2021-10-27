00581def64a60fd03659d3dc103ef604003e81a7
Huadong Zhang <407108459@qq.com>
2021-10-27 14:43:22 -0400

    FrogBossAnimatorController

---
+++ Assets/Scripts/Enemies/BossFrogAnimatorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFrogAnimatorController : MonoBehaviour
+{
+    public Animator animator;
+    public void Attack1()
+    {
+        animator.SetTrigger("Attack1");
+    }
+
+    public void Attack2()
+    {
+        animator.SetTrigger("Attack2");
+    }
+
+    public void Attack3()
+    {
+        animator.SetTrigger("Attack3");
+    }
+
+    public void Attack4()
+    {
+        animator.SetTrigger("Attack4");
+    }
+
+    public void Hurt()
+    {
+        animator.SetTrigger("Hurt");
+    }
+
+    public void StartCoolDown()
+    {
+        animator.SetBool("IsCoolingDown", true);
+    }
+
+    public void EndCoolDown()
+    {
+        animator.SetBool("IsCoolingDown", false);
+    }
+}
