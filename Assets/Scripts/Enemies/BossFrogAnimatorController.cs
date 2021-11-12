bb06fab8e337506b699cbdff0eca620c0407d90c
Huadong Zhang <407108459@qq.com>
2021-11-12 14:59:42 -0500

    Boss3 Shadow

--- Assets/Scripts/Enemies/BossFrogAnimatorController.cs
+++ Assets/Scripts/Enemies/BossFrogAnimatorController.cs
@@ -5,42 +5,50 @@ using UnityEngine;
 public class BossFrogAnimatorController : MonoBehaviour
 {
     public Animator animator;
+    public Animator shadowAnimator;
     public GameObject brainCollider;
 
     public void Attack1()
     {
         animator.SetTrigger("Attack1");
+        shadowAnimator.SetTrigger("Attack1");
     }
 
     public void Attack2()
     {
         animator.SetTrigger("Attack2");
+        shadowAnimator.SetTrigger("Attack2");
     }
 
     public void Attack3()
     {
         animator.SetTrigger("Attack3");
+        shadowAnimator.SetTrigger("Attack3");
     }
 
     public void Attack4()
     {
         animator.SetTrigger("Attack4");
+        shadowAnimator.SetTrigger("Attack4");
     }
 
     public void Hurt()
     {
         animator.SetTrigger("Hurt");
+        shadowAnimator.SetTrigger("Hurt");
     }
 
     public void StartCoolDown()
     {
         animator.SetBool("IsCoolingDown", true);
+        shadowAnimator.SetBool("IsCoolingDown", true);
         brainCollider.SetActive(true);
     }
 
     public void EndCoolDown()
     {
         animator.SetBool("IsCoolingDown", false);
+        shadowAnimator.SetBool("IsCoolingDown", false);
         brainCollider.SetActive(false);
     }
 }
